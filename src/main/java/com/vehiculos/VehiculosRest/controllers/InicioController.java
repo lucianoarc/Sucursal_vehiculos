bef6aae528768c2df23a23c55df4916468da8757
lucianoarc <165394625+lucianoarc@users.noreply.github.com>
2025-02-11 16:42:20 -0300

    Update InicioController.java

--- src/main/java/com/vehiculos/VehiculosRest/controllers/InicioController.java
+++ src/main/java/com/vehiculos/VehiculosRest/controllers/InicioController.java
@@ -9,7 +9,7 @@ import java.util.Calendar;
 public class InicioController {
 
 
-    private String version = "1.1";
+    private String version = "1.5";
     @GetMapping("/")
     public String comienzo(){
         Calendar calendario = Calendar.getInstance();
